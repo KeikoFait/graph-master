a5fbdf6ec1754fcfa0311c78d347f88c3092e2e5
renalage <renalage@gmail.com>
2012-11-13 17:16:18 +0000

    The outputInfo method for adjacency matrix also implemented.

--- Graph.cpp
+++ Graph.cpp
@@ -2,8 +2,9 @@
 
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <vector>
-#include <cmath>
+
 
 using namespace std;
 
@@ -11,7 +12,6 @@ Graph::Graph()
 {
 	numV = 0;
 	numA = 0;
-	//medGrau = 0.0;
 }
 
 
@@ -19,14 +19,15 @@ Graph::~Graph()
 {
 	delete adjMatrix;
 	delete adjList;
-	//delete distEmp;
 }
 
 bool Graph::makeGraph(string url, unsigned short int repr)
 {	
+	//Variables for connected vertices
 	unsigned long long v1;
-	unsigned long long v2;		//Variables for connected vertices	ifstream file
+	unsigned long long v2;
 	ifstream file;
+	reprStruct = repr;
 
 	//Opening and reading file
 	file.open(url);
@@ -36,13 +37,13 @@ bool Graph::makeGraph(string url, unsigned short int repr)
 		cin.ignore();
 		return false;
 	}
-
 	//Read first line with number of vertices
 	file >> numV;
 
-	if (repr == 1)
-	//Store on adjacency matrix
+	if (reprStruct == 1)
+	//Represent graph as an adjacency matrix
 	{
+		delete adjList;
 		adjMatrix = new vector<bool> (numV*numV, false);
 		while (!file.eof())
 		{
@@ -51,7 +52,11 @@ bool Graph::makeGraph(string url, unsigned short int repr)
 			adjMatrix->at( getPos( &v1, &v2) ) = true;
 			adjMatrix->at( getPos( &v2, &v1) ) = true;
 		}
-	} else {
+	} 
+	else
+	//Represent graph as an adjacency list
+	{
+		delete adjMatrix;
 		adjList = new vector< vector<unsigned long long> >(numV, vector<unsigned long long>());
 		while (!file.eof())
 		{
@@ -59,78 +64,89 @@ bool Graph::makeGraph(string url, unsigned short int repr)
 			numA++;
 			adjList->at(v1-1).push_back(v2);
 			adjList->at(v2-1).push_back(v1);
-		}	
+		}
 	}
-
 	file.close();
-	
-	//Output info about graph to a file
-	outputInfo(repr);
-
 	return true;
 }
 
-bool Graph::outputInfo(unsigned short int repr)
+bool Graph::outputInfo()
 {
 	ofstream file;
 
-	//Opening and reading file
+	//Opening file for write
 	file.open("graphInfo.txt");
 	if (!file.is_open())
 	{
-		cout << "Error while opening file!" << endl;
+		cerr << "Error while opening file!" << endl;
 		cin.ignore();
 		return false;
 	}
-
+	//Writing number of vertices and edges to file
 	file << "n = " << numV << endl;
-	file << "a = " << numA << endl;
+	file << "m = " << numA << endl;
 	
-	if (repr == 1)
+	vector<unsigned long long> distEmp; // Will store the empirical distribution of degrees
+
+	if (reprStruct == 1)
 	{
-	} 
-	else
-	{
-		vector<unsigned long long>* distEmp = distEmpList(adjList);
-		
-		file << "d_medio = " << avgDegreeList(adjList) << endl;
- 		for (unsigned long long i = 0; i < distEmp->size(); i++)
+		//Output degree information to file if adjacency matrix structure is utilized
+		file << "d_medio = " << fixed << setprecision(1) << degreeInfoMatrix(adjMatrix, &distEmp) << endl;
+ 		for (unsigned long long i = 0; i < distEmp.size(); i++)
  		{
- 			file << i << " " << distEmp->at(i) << endl;
+ 			file << i << " " << fixed << setprecision(2) << (double)distEmp.at(i)/numV << endl;
  		}
- 		delete distEmp;
+	} else
+	{	
+		//Output degree information to file if adjacency list structure is utilized
+		file << "d_medio = " << fixed << setprecision(1) << degreeInfoList(adjList, &distEmp) << endl;
+ 		for (unsigned long long i = 0; i < distEmp.size(); i++)
+ 		{
+ 			file << i << " " << fixed << setprecision(2) << (double)distEmp.at(i)/numV << endl;
+ 		}
 	}
-
 	file.close();
-
 	return true;
 }
 
-double Graph::avgDegreeList(vector<vector<unsigned long long> >* aList)
+double Graph::degreeInfoList(vector<vector<unsigned long long> >* aList, vector<unsigned long long>* distEmp)
 {
 	double med = 0;
-	for(int i = 0; i < aList->size(); i++){
+	for(unsigned long long i = 0; i < aList->size(); i++)
+	{
 		med += aList->at(i).size();
+		if (aList->at(i).size() > distEmp->size()) 
+			distEmp->resize(aList->at(i).size() + 1);
+		distEmp->at(aList->at(i).size()) +=  1;
 	}
 	med /= aList->size();
 	return med;
 }
 
-vector <unsigned long long>* Graph::distEmpList(vector <vector<unsigned long long> >* aList)
+double Graph::degreeInfoMatrix(vector<bool>* aMatrix, vector<unsigned long long>* distEmp)
 {
-	vector<unsigned long long>* distEmp = new vector<unsigned long long>;
-
-	for(int i = 0; i < aList->size(); i++){
-		if (aList->at(i).size() > distEmp->size()) distEmp->resize(aList->at(i).size() + 1);
-		distEmp->at(aList->at(i).size()) +=  1;
+	double med = 0;
+	unsigned long long degree = 0;
+	for (unsigned long long i = 0; i < aMatrix->size(); i++)
+	{
+		if (aMatrix->at(i) == true)
+			degree++;
+		if ((i + 1) % numV == 0)
+		{
+			med += degree;
+			if (degree > distEmp->size())
+				distEmp->resize(degree + 1);
+			distEmp->at(degree) +=  1;
+			degree = 0;
+		}
 	}
-
-	return distEmp;
+	med /= numV;
+	return med;
 }
 
 unsigned long long Graph::getPos(unsigned long long* x, unsigned long long* y)
 {
-	//Access value in matrix: adjMatrix (i*size + j)
+	//Access value in matrix: adjMatrix (i*size + j); -1 because the vertices starts at 1 and indexes at 0
 	return ((*x-1) * numV + (*y-1));
 }
 
@@ -166,7 +182,9 @@ int main()
 {
 	cout << "rodou!" << endl;
 	Graph* graph = new Graph();
-	graph->makeGraph("as_graph.txt", 2);
+	graph->makeGraph("teste_graph.txt", 1);
+	//Output info about graph to a file
+	graph->outputInfo();
 	cin.ignore();
 
 	return 0;
